ffd57fe619d89a968f94f4327479a107cc30966a
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-07-20 01:30:15 +0300

    low-high-price-add-js-basket-update

--- app.js
+++ app.js
@@ -19,6 +19,47 @@ fetch("/games.json")
         }).join("");
         document.querySelector("#pc").innerHTML = pcData;
 
+        //! PC GAME LOW-HIGH PRICE
+        let lowPc = document.querySelector("#lowPc");
+        let highPc = document.querySelector("#highPc");
+        lowPc.addEventListener("click", () => {
+            let low = onlyPc.sort((a, b) => a.standart.price - b.standart.price);
+            let LowPcGame = low.map(element => {
+                return `<div class="mt-5 mb-5 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="pc-game d-flex flex-column align-items-center">
+                    <div class="p-4 d-flex flex-column align-items-center">
+                        <img src="${element.standart.img}" width="100%" height="400px">
+                        <h1 class="text-center mt-2">${element.standart.name}</h1>
+                        <h2 class="text-center price mt-2">${element.standart.price}$</h2>
+                        <a href="/detail.html?id=${element.id}" class="btn btn-light">Go detail</a>
+                        <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                    </div>
+                </div>
+            </div>
+                `;
+            }).join("");
+            document.querySelector("#pc").innerHTML = LowPcGame;
+        });
+        highPc.addEventListener("click", () => {
+            onlyPc.sort((a, b) => a.standart.price - b.standart.price);
+            let high = onlyPc.reverse();
+            let HighPcGame = high.map(element => {
+                return `<div class="mt-5 mb-5 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="pc-game d-flex flex-column align-items-center">
+                    <div class="p-4 d-flex flex-column align-items-center">
+                        <img src="${element.standart.img}" width="100%" height="400px">
+                        <h1 class="text-center mt-2">${element.standart.name}</h1>
+                        <h2 class="text-center price mt-2">${element.standart.price}$</h2>
+                        <a href="/detail.html?id=${element.id}" class="btn btn-light">Go detail</a>
+                        <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                    </div>
+                </div>
+            </div>
+                `;
+            }).join("");
+            document.querySelector("#pc").innerHTML = HighPcGame;
+        });
+
         //! PC GAME SEARCH FILTERED
         let pcSearch = document.querySelector("#pc_search");
         pcSearch.addEventListener("input", (e) => {
@@ -60,7 +101,11 @@ fetch("/games.json")
             let basketGame = basketItems.map(element => {
                 return `
       <div class="basket-game m-auto mt-3 p-2 row">
-        <div class="basket-left col-xxl-4 col-xl-4">
+        <div class="basket-left col-xxl-4 col-xl-4" style="background-image: url(${element.standart.img});
+        background-position: top;
+        background-size: cover;
+        border-radius: 15px;
+        border: 2px solid black;">
         </div>
         <div class="basket-right d-flex flex-column justify-content-center align-items-center col-8">
           <h3>${element.standart.name}</h3>
@@ -120,6 +165,47 @@ fetch("/games.json")
         }).join("");
         document.querySelector("#ps5").innerHTML = ps5Data;
 
+        //! PS5 GAME LOW-HIGH PRICE
+        let lowPs5 = document.querySelector("#lowPs5");
+        let highPs5 = document.querySelector("#highPs5");
+        lowPs5.addEventListener("click", () => {
+            let low = onlyPs5.sort((a, b) => a.standart.price - b.standart.price);
+            let LowPs5Game = low.map(element => {
+                return `<div class="mt-5 mb-5 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="pc-game d-flex flex-column align-items-center">
+                    <div class="p-4 d-flex flex-column align-items-center">
+                        <img src="${element.standart.img}" width="100%" height="400px">
+                        <h1 class="text-center mt-2">${element.standart.name}</h1>
+                        <h2 class="text-center price mt-2">${element.standart.price}$</h2>
+                        <a href="/detail.html?id=${element.id}" class="btn btn-light">Go detail</a>
+                        <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                    </div>
+                </div>
+            </div>
+                `;
+            }).join("");
+            document.querySelector("#ps5").innerHTML = LowPs5Game;
+        });
+        highPs5.addEventListener("click", () => {
+            onlyPs5.sort((a, b) => a.standart.price - b.standart.price);
+            let high = onlyPs5.reverse();
+            let HighPs5Game = high.map(element => {
+                return `<div class="mt-5 mb-5 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="pc-game d-flex flex-column align-items-center">
+                    <div class="p-4 d-flex flex-column align-items-center">
+                        <img src="${element.standart.img}" width="100%" height="400px">
+                        <h1 class="text-center mt-2">${element.standart.name}</h1>
+                        <h2 class="text-center price mt-2">${element.standart.price}$</h2>
+                        <a href="/detail.html?id=${element.id}" class="btn btn-light">Go detail</a>
+                        <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                    </div>
+                </div>
+            </div>
+                `;
+            }).join("");
+            document.querySelector("#ps5").innerHTML = HighPs5Game;
+        });
+
         //! PS5 GAME SEARCH FILTERED
         let ps5Search = document.querySelector("#ps5_search");
         ps5Search.addEventListener("input", (e) => {
@@ -161,7 +247,11 @@ fetch("/games.json")
             let basketGame = basketItems.map(element => {
                 return `
               <div class="basket-game m-auto mt-3 p-2 row">
-                <div class="basket-left col-xxl-4 col-xl-4">
+                <div class="basket-left col-xxl-4 col-xl-4" style="background-image: url(${element.standart.img});
+                background-position: top;
+                background-size: cover;
+                border-radius: 15px;
+                border: 2px solid black;">
                 </div>
                 <div class="basket-right d-flex flex-column justify-content-center align-items-center col-8">
                   <h3>${element.standart.name}</h3>
@@ -221,6 +311,47 @@ fetch("/games.json")
         }).join("");
         document.querySelector("#xbox").innerHTML = xboxData;
 
+        //! XBOX GAME LOW-HIGH PRICE
+        let lowXbox = document.querySelector("#lowXbox");
+        let highXbox = document.querySelector("#highXbox");
+        lowXbox.addEventListener("click", () => {
+            let low = onlyXbox.sort((a, b) => a.standart.price - b.standart.price);
+            let LowXboxGame = low.map(element => {
+                return `<div class="mt-5 mb-5 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="pc-game d-flex flex-column align-items-center">
+                    <div class="p-4 d-flex flex-column align-items-center">
+                        <img src="${element.standart.img}" width="100%" height="400px">
+                        <h1 class="text-center mt-2">${element.standart.name}</h1>
+                        <h2 class="text-center price mt-2">${element.standart.price}$</h2>
+                        <a href="/detail.html?id=${element.id}" class="btn btn-light">Go detail</a>
+                        <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                    </div>
+                </div>
+            </div>
+                `;
+            }).join("");
+            document.querySelector("#xbox").innerHTML = LowXboxGame;
+        });
+        highXbox.addEventListener("click", () => {
+            onlyXbox.sort((a, b) => a.standart.price - b.standart.price);
+            let high = onlyXbox.reverse();
+            let HighXboxGame = high.map(element => {
+                return `<div class="mt-5 mb-5 col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="pc-game d-flex flex-column align-items-center">
+                    <div class="p-4 d-flex flex-column align-items-center">
+                        <img src="${element.standart.img}" width="100%" height="400px">
+                        <h1 class="text-center mt-2">${element.standart.name}</h1>
+                        <h2 class="text-center price mt-2">${element.standart.price}$</h2>
+                        <a href="/detail.html?id=${element.id}" class="btn btn-light">Go detail</a>
+                        <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                    </div>
+                </div>
+            </div>
+                `;
+            }).join("");
+            document.querySelector("#xbox").innerHTML = HighXboxGame;
+        });
+
         //! XBOX GAME SEARCH FILTERED
         let xboxSearch = document.querySelector("#xbox_search");
         xboxSearch.addEventListener("input", (e) => {
@@ -262,7 +393,11 @@ fetch("/games.json")
             let basketGame = basketItems.map(element => {
                 return `
               <div class="basket-game m-auto mt-3 p-2 row">
-                <div class="basket-left col-xxl-4 col-xl-4">
+                <div class="basket-left col-xxl-4 col-xl-4" style="background-image: url(${element.standart.img});
+                background-position: top;
+                background-size: cover;
+                border-radius: 15px;
+                border: 2px solid black;">
                 </div>
                 <div class="basket-right d-flex flex-column justify-content-center align-items-center col-8">
                   <h3>${element.standart.name}</h3>
@@ -320,7 +455,7 @@ fetch("/games.json")
 
             <div class="detail-content mt-5 p-3">
 
-                <h1 class="text-center brand">${element.standart.name}</h1>
+                <h1 class="text-center brand mt-5">${element.standart.name}</h1>
                 <div class="detail-top-row mt-5 mb-5 row justify-content-center">
                     <div class="mt-3 mb-3 col-xxl-2 col-xl-2 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="left-detail d-flex justify-content-center" style="width: 100%;
@@ -376,7 +511,7 @@ fetch("/games.json")
                                             minima porro tempore.</li>
                                     </ul>
                                     <h2 class="text-center price mt-2">${element.standart.price}$</h2>
-                                    <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                                    <button id="STANDART" class="btn btn-danger mt-2" value="${element.standart.name}">Add Basket</button>
                                 </div>
                             </div>
                         </div>
@@ -402,7 +537,7 @@ fetch("/games.json")
                                             minima porro tempore.</li>
                                     </ul>
                                     <h2 class="text-center price mt-2">${element.deluxe.price}$</h2>
-                                    <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                                    <button id="DELUXE" class="btn btn-danger mt-2" value="${element.deluxe.name}">Add Basket</button>
                                 </div>
                             </div>
                         </div>
@@ -428,7 +563,7 @@ fetch("/games.json")
                                             minima porro tempore.</li>
                                     </ul>
                                     <h2 class="text-center price mt-2">${element.ultimate.price}$</h2>
-                                    <button id="${element.id}" class="btn btn-danger mt-2">Add Basket</button>
+                                    <button id="COLLECTOR" class="btn btn-danger mt-2" value="${element.ultimate.name}">Add Basket</button>
                                 </div>
                             </div>
                         </div>
@@ -466,6 +601,84 @@ fetch("/games.json")
         `;
         }).join("");
         document.querySelector("#detail").innerHTML = goDetail;
+
+        //! DETAIL BASKET
+        let basketItems = []; // Sepetteki ürünleri tutan liste
+
+        // Sayfa yüklendiğinde local storage'dan sepet içeriğini al ve güncelle
+        let savedBasket = localStorage.getItem("Saved Basket");
+        basketItems = JSON.parse(savedBasket);
+        updateBasket();
+
+        function saveBasket() {
+            let savedBasket = JSON.stringify(basketItems);
+            localStorage.setItem("Saved Basket", savedBasket);
+        }
+        let addStandart = document.getElementById("STANDART");
+        let addDeluxe = document.getElementById("DELUXE");
+        let addUltimate = document.getElementById("COLLECTOR");
+        // Sepet içeriğini güncelleyen fonksiyon
+        function updateBasket(e) {
+            let basketGame = basketItems.map(element => {
+                return `
+                  <div class="basket-game m-auto mt-3 p-2 row">
+                    <div class="basket-left col-xxl-4 col-xl-4" style="background-image: url(${element.standart.img});
+                    background-position: top;
+                    background-size: cover;
+                    border-radius: 15px;
+                    border: 2px solid black;">
+                    </div>
+                    <div class="basket-right d-flex flex-column justify-content-center align-items-center col-8">
+                      <h3>${element.standart.name}</h3>
+                      <h4>${element.standart.edition}</h4>
+                      <h4>${element.standart.price}</h4>
+                      <button class="btn btn-danger remove-button">Remove Basket</button>
+                    </div>
+                  </div>`;
+            }).join("");
+            document.querySelector(".basket-content").innerHTML = basketGame;
+
+            // Remove butonlarına dinleyici ekleme
+            let removeGame = document.querySelectorAll(".remove-button");
+            removeGame.forEach((button, index) => {
+                button.addEventListener("click", () => {
+                    basketItems.splice(index, 1); // Ürünü sepet listesinden kaldırma
+                    updateBasket(e); // Sepet içeriğini güncelleme
+                    saveBasket();
+                });
+            });
+        }
+
+        addStandart.addEventListener("click", (e) => {
+            console.log(e.target.value);
+            let addStandart = data.find(item => item.standart.edition == e.target.id && item.standart.name == e.target.value); // Tekil ürünü bulma
+            console.log(addStandart);
+            basketItems.push(addStandart); // Ürünü sepet listesine ekleme
+            // Sepet içeriğini güncelleyen fonksiyonu çağırma
+            updateBasket(e);
+            // Sepet içeriğini local storage'a kaydet
+            saveBasket();
+        });
+        addDeluxe.addEventListener("click", (e) => {
+            console.log(e.target.value);
+            let addDeluxe = data.find(item => item.deluxe.edition == e.target.id && item.deluxe.name == e.target.value); // Tekil ürünü bulma
+            console.log(addDeluxe);
+            basketItems.push(addDeluxe); // Ürünü sepet listesine ekleme
+            // Sepet içeriğini güncelleyen fonksiyonu çağırma
+            updateBasket(e);
+            // Sepet içeriğini local storage'a kaydet
+            saveBasket();
+        });
+        addUltimate.addEventListener("click", (e) => {
+            console.log(e.target.value);
+            let addUltimate = data.find(item => item.ultimate.edition == e.target.id && item.ultimate.name == e.target.value); // Tekil ürünü bulma
+            console.log(addUltimate);
+            basketItems.push(addUltimate); // Ürünü sepet listesine ekleme
+            // Sepet içeriğini güncelleyen fonksiyonu çağırma
+            updateBasket(e);
+            // Sepet içeriğini local storage'a kaydet
+            saveBasket();
+        });
     });
 
 //! BASKET => Navbar
